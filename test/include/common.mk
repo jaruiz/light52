18b7406b9596078306990c11a6f322c2f6e9bf78
Jose A. Ruiz <ja_rd@outlook.com>
2018-09-15 17:22:13 +0100

    Update the common makefile fragment that builds the test programs.
    Eventually we want to use as much as SDCC as possible, including the
    assembler if possible.
    The opcode tester still needs ASEM51 and can't use SDCC's assembler.

--- test/include/common.mk
+++ test/include/common.mk
@@ -4,12 +4,15 @@
 # project configuration variables have been defined. See the Dhrystone makefile 
 # for an usage example.
 
-# (Assembly source tests use common makefile 'assembler.mk' and not this one.)
+# Some assembly source tests may choose to use makefile fragment 'asem51.mk' 
+# and not this one. if they need features missing from sdas8051 2.0.
+# (The version of sdcc commonly found in repos as of Spring'18 ships with a 
+# very old 8051 assembler that has trouble with local macro symbols etc.)
 
 #-- Toolchain executables ------------------------------------------------------
 
 # WARNING: Not tested on Windows!
-AS = 
+AS = sdas8051
 CC = sdcc
 LD = sdcc
 RM = rm -rf
@@ -25,9 +28,13 @@ SRCDIR = src
 VHDL_TB_PATH = .
 COMDIR = ../common
 
+# FIXME make conditional
+CDEBUGFLAGS = --debug --noinduction --nooverlay
+LDEBUGFLAGS = --debug
+
 # Toolchain flags
-LFLAGS += -o $(OBJDIR)/ --code-size $(XCODE_SIZE) --xram-size $(XDATA_SIZE) 
-CFLAGS += -o $(OBJDIR)/ -D__LIGHT52__=1
+LFLAGS += -o $(OBJDIR)/ --code-size $(XCODE_SIZE) --xram-size $(XDATA_SIZE) $(LDEBUGFLAGS)
+CFLAGS += -o $(OBJDIR)/ -D__LIGHT52__=1 $(CDEBUGFLAGS)
 AFLAGS += -c
 
 # The SW sources will need to know if we're building for actual HW.
@@ -46,8 +53,10 @@ VPATH := $(dir $(SRC))
 
 # If no list of sources is specified, just compile all c files in src.
 TEST_SRC ?= $(wildcard $(SRCDIR)/*.c)
+TEST_ASM ?= $(wildcard $(SRCDIR)/*.asm)
 COMM_SRC ?= $(wildcard $(COMDIR)/*.c)
-TEST_OBJS ?= $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.rel, $(TEST_SRC))
+TEST_OBJS = $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.rel, $(TEST_SRC))
+TEST_OBJS += $(patsubst $(SRCDIR)/%.asm, $(OBJDIR)/%.rel, $(TEST_ASM))
 COMM_OBJS ?= $(patsubst $(COMDIR)/%.c, $(OBJDIR)/%.rel, $(COMM_SRC))
 OBJS = $(TEST_OBJS) $(COMM_OBJS)
 # Default name of hex file.
@@ -73,6 +82,10 @@ $(OBJDIR)/%.rel : $(SRCDIR)/%.c $(OBJDIR)
 $(OBJDIR)/%.rel : $(COMDIR)/%.c
 	$(CC) $(CFLAGS) -c $<
 
+# Assemble asm sources into relocatable object files
+$(OBJDIR)/%.rel : $(SRCDIR)/%.asm $(OBJDIR)
+	$(AS) -I../include -l -o $@ $<
+
 
 $(BINDIR):
 	mkdir -p $(BINDIR)
@@ -80,7 +93,7 @@ $(BINDIR):
 # Build executable file and move it to the bin directory
 $(BINDIR)/$(BIN): $(OBJS)
 	@echo Building executable file $@ ...
-	$(LD) $(OBJS) $(LFLAGS)
+	$(LD) $(OBJS) $(LFLAGS) --code-loc 0
 	$(CP) $(OBJDIR)/*.ihx $(BINDIR)/$(BIN)
 
 # Root target
